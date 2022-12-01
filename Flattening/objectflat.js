eafc8ee6103a333afe2f5d40c04e5b35edfece95
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-02 00:24:21 +0530

    Added initial logic for the objectflat file.

--- Flattening/objectflat.js
+++ Flattening/objectflat.js
@@ -1,1 +1,21 @@
 // Flattening of an object recursively
+
+let obj = {
+  name: "abc",
+  nos: {
+    address: "123 lane",
+    pin: 123,
+    phone: 00000,
+  },
+  key: "pan",
+  selection: {
+    members: {
+      ide: 1,
+      rating: 4.6,
+      info: "shane",
+      group: "2t",
+    },
+  },
+};
+
+// function to flatten obj recursively
