52f888cb9ba43a3921118016bf811fd92c39e269
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-03 20:22:06 +0530

    Added initial logic for the map polyfill method

--- Polyfills/map_polyfill.js
+++ Polyfills/map_polyfill.js
@@ -5,4 +5,12 @@ Array.prototype.myMap = function (cb) {
   for (let i = 0; i < this.length; i++) {
     resArr.push(cb(this[i], i, this));
   }
+  return resArr;
 };
+
+let arr = [1, 2, 3, 4, 5];
+const result = arr.myMap((ele, idx, arr) => {
+  return ele % 2;
+});
+
+console.log(result);
