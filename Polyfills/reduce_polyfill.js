772f918034144cb93d3936783d6497ecd2460181
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-06 00:38:40 +0530

    Added final customreduce logic for the reduce polyfill method.

--- Polyfills/reduce_polyfill.js
+++ Polyfills/reduce_polyfill.js
@@ -7,13 +7,26 @@ Array.prototype.myreduce = function (cb, initialValue) {
   let res = initialValue;
 
   for (let i = 0; i < this.length; i++) {
-    if (res == undefined) {
+    if (res === undefined) {
       res = this[i];
+    } else {
+      res = cb(res, this[i], i, this);
     }
-    res = cb(res, this[i], i, this);
   }
 
   return res;
 };
 
-console.log(arr.myreduce((acc, val) => acc + val, 0));
+console.log(
+  arr.myreduce((acc, val) => {
+    if (acc < val) {
+      acc = val;
+    }
+    return acc;
+  }, 100)
+);
+console.log(
+  arr.myreduce((acc, val) => {
+    if (acc < val) return val;
+  })
+);
