443787a85f639374242445f3b682447a3fe28a49
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-05 23:32:03 +0530

    Added custom filterlogic for custom filter function

--- Polyfills/filter_polyfill.js
+++ Polyfills/filter_polyfill.js
@@ -1,4 +1,14 @@
 // Implementation for custom filter function
 
 // example array
-let arr = ["a", "b", "c", "d", "e", "f"];
+let arrch = ["a", "b", "c", "d", "e", "f"];
+
+Array.prototype.myFilter = function (cb) {
+  let resArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      resArr.push(this[i]);
+    }
+  }
+  return resArr;
+};
