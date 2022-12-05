dca94a59c02c8cdee0bc9b1bf3ee87d9f3ca366d
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-05 23:21:02 +0530

    Added foreach logic for the foreach implementation

--- Polyfills/forEach_polyfill.js
+++ Polyfills/forEach_polyfill.js
@@ -2,3 +2,10 @@
 
 // test array
 let arr = [1, 2, 3, 4, 5];
+
+// Implentation
+Array.prototype.myForEach = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    cb(this[i], i, this);
+  }
+};
