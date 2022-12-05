1eb2544ab9af21154f727aa7feb383a707cdebb7
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-06 00:06:35 +0530

    Added intial customreduce logic for the reduce polyfill method.

--- Polyfills/reduce_polyfill.js
+++ Polyfills/reduce_polyfill.js
@@ -1,1 +1,12 @@
 // custom reduce polyfill method
+
+// example array
+let arr = [140, 120, 52, 48, 90];
+
+Array.prototype.myreduce = function (cb, intialValue) {
+  let res = intialValue;
+
+  for (let i = 0; i < this.length; i++) {
+    res = res || this[i];
+  }
+};
