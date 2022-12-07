14ec347d646ac227426bb601fc86ff4407a28c4f
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-07 23:20:48 +0530

    Added intial logic for the find polyfill file

--- Polyfills/find_polyfill.js
+++ Polyfills/find_polyfill.js
@@ -2,3 +2,10 @@
 
 // example array
 let arr = [5, 8, 15, 20, 39];
+
+// polyfill implementation
+Array.prototype.myFind = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    let result = cb(this[i], i, this);
+  }
+};
