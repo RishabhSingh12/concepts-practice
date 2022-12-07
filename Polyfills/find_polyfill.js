e40bda24d5e4f79441d788e2241c19ea82567388
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-07 23:22:01 +0530

    Modified intial logic for the find polyfill file

--- Polyfills/find_polyfill.js
+++ Polyfills/find_polyfill.js
@@ -7,5 +7,9 @@ let arr = [5, 8, 15, 20, 39];
 Array.prototype.myFind = function (cb) {
   for (let i = 0; i < this.length; i++) {
     let result = cb(this[i], i, this);
+    if (result) {
+      return this[i];
+    }
+    return undefined;
   }
 };
