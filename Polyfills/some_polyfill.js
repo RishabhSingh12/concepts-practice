3efbfb428838247289c0b79c508b579e2b185c7d
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-08 00:23:34 +0530

    Added initial logic for some polyfill method

--- Polyfills/some_polyfill.js
+++ Polyfills/some_polyfill.js
@@ -5,5 +5,9 @@ let array = ["alex", "sharon", "bob", "alexa"];
 
 // polyfill implementation
 Array.prototype.mySome = function (cb) {
-  for (let i = 0; i < this.length; i++) {}
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      return true;
+    }
+  }
 };
