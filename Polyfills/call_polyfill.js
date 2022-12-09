e06002b7b865ef3be9ac62469909d82a9a9dbc99
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-09 23:40:14 +0530

    Added initial assets and files for the call polyfill method

--- Polyfills/call_polyfill.js
+++ Polyfills/call_polyfill.js
@@ -1,8 +1,12 @@
 // custom implementation for the call method
 
 // example object
-
 let car = {
   color: "red",
   company: "rivian",
 };
+
+// example function
+function purchaseCar(price) {
+  console.log(`I purchased ${this.color} - ${this.company} for Rs ${price}`);
+}
