2709876dae34c4e98b823d9fba1a3c1e55378f7d
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-10 23:33:43 +0530

    Added initial logic for the custom bind implementation

--- Polyfills/function_polyfills/bind_polyfill.js
+++ Polyfills/function_polyfills/bind_polyfill.js
@@ -1,1 +1,11 @@
 // bind polyfill implementation
+
+let car = {
+  color: "red",
+  company: "rivian",
+};
+
+// example function
+function purchaseCar(price) {
+  console.log(`I purchased ${this.color} - ${this.company} for Rs ${price}`);
+}
