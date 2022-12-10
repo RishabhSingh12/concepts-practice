62376fdd894a914be29b31cc5ee5f018088acfe3
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-10 23:41:04 +0530

    Added final logic for the custom bind implementation

--- Polyfills/function_polyfills/bind_polyfill.js
+++ Polyfills/function_polyfills/bind_polyfill.js
@@ -16,4 +16,9 @@ Function.prototype.myBind = function (context = {}, ...args) {
   if (typeof this !== "function") {
     throw new Error(this + "cannot be bound as it's not callable");
   }
+
+  context.fn = this;
+  return function (...newArgs) {
+    context.fn(...args, ...newArgs);
+  };
 };
