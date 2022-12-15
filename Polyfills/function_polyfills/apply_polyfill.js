c070e4de2107e569b6a1302eb8641d803adbe7bd
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-15 22:56:15 +0530

    Added intial logic for apply polyfill method implementation.

--- Polyfills/function_polyfills/apply_polyfill.js
+++ Polyfills/function_polyfills/apply_polyfill.js
@@ -5,3 +5,8 @@ let car = {
   color: "red",
   company: "rivian",
 };
+
+// example function
+function purchaseCar(price) {
+  console.log(`I purchased ${this.color} - ${this.company} for Rs ${price}`);
+}
