c2c215d772fc4fe01a81c5b8be8220361e144fb2
Rishabh Kumar <rishabh2807s@gmail.com>
2022-12-15 23:01:51 +0530

    Added logic for apply polyfill method implementation.

--- Polyfills/function_polyfills/apply_polyfill.js
+++ Polyfills/function_polyfills/apply_polyfill.js
@@ -17,6 +17,10 @@ Function.prototype.myApply = function (context = {}, args = []) {
     throw new Error(this + "It's not callable");
   }
 
+  if (!Array.isArray(args)) {
+    throw new TypeError("CreateListFromArrayLike called on non-object");
+  }
+
   context.fn = this;
   context.fn(...args);
 };
