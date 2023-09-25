3fac9c2f20701e692ea921b34d4371d35e95c5ca
Rishabh Kumar <rishabh2807s@gmail.com>
2023-09-25 23:59:02 +0530

    Added complete implementation of promise.any polyfill in js

--- Polyfills/Promise-polyfills/promise-any-polyfill.js
+++ Polyfills/Promise-polyfills/promise-any-polyfill.js
@@ -4,4 +4,31 @@ const myPromiseAny = function (promisesArr) {
   let promiseError = new Array(promisesArr.length);
 
   let counter = 0;
+
+  return new Promise((resolve, reject) => {
+    promisesArr.forEach((promise, idx) => {
+      Promise.resolve(promise)
+        .then(resolve)
+        .catch((error) => {
+          promiseError[counter] = error;
+          counter += 1;
+          if (counter === promisesArr.length) {
+            //all promises rejected, reject outer promise
+            reject(promiseError);
+          }
+        });
+    });
+  });
 };
+
+//Inputs for testing"
+const test1 = new Promise((res, rej) => {
+  setTimeout(rej, 500, "one");
+});
+const test2 = new Promise((res, rej) => {
+  setTimeout(rej, 300, "two");
+});
+
+myPromiseAny([test1, test2])
+  .then((val) => console.log(val))
+  .catch((err) => console.log(err));
